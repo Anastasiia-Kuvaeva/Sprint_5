b7b5e682ffa8dd36b05a912fb3052d48cbf59d9a
Anastasiia-Kuvaeva <136185788+Anastasiia-Kuvaeva@users.noreply.github.com>
2024-03-12 23:16:01 +0300

    sprint_5 исправление замечаний

--- conftest.py
+++ conftest.py
@@ -4,24 +4,44 @@ from selenium.webdriver.common.by import By
 import configuration
 import data
 import locators
-import time
-@pytest.fixture
-def authorization():
-    # Фикстура авторизации
-    def authorizationInner():
-        # Открываем браузер
-        driver = webdriver.Chrome()
-        # Открываем страницу
-        driver.get(configuration.URL_LOGIN)
-        # получение полей формы авторизации
-        email_input = driver.find_element(By.XPATH, locators.LOGIN_FORM_EMAIL_INPUT)
-        password_input = driver.find_element(By.XPATH, locators.LOGIN_FORM_PASSWORD_INPUT)
-        submit_button = driver.find_element(By.XPATH, locators.LOGIN_FORM_SUBMIT_BUTTON)
-        # заполнение полей формы авторизации
-        email_input.send_keys(data.USER_EMAIL)
-        password_input.send_keys(data.USER_PASSWORD)
-        submit_button.click()
-        time.sleep(1)  # Ожидаем перехода на новую страницу
-        return driver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import random
 
-    return authorizationInner
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="chrome", type=str)
+
+# Фикстура получения и закрытия driver
+@pytest.fixture
+def driver(request):
+    browser = request.config.getoption("--browser")
+    driver = None
+    if browser == "firefox":
+        driver = webdriver.Firefox()
+    elif browser == "chrome":
+        driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+# Фикстура генерации email
+@pytest.fixture
+def email():
+    return "akuvaeva" + str(random.randint(1000, 9999)) + "@yandex.com"
+
+
+# Фикстура авторизации
+@pytest.fixture
+def authorization(driver):
+    # Открываем страницу
+    driver.get(configuration.URL_LOGIN)
+    # получение полей формы авторизации
+    email_input = driver.find_element(By.XPATH, locators.LOGIN_FORM_EMAIL_INPUT)
+    password_input = driver.find_element(By.XPATH, locators.LOGIN_FORM_PASSWORD_INPUT)
+    submit_button = driver.find_element(By.XPATH, locators.LOGIN_FORM_SUBMIT_BUTTON)
+    # заполнение полей формы авторизации
+    email_input.send_keys(data.USER_EMAIL)
+    password_input.send_keys(data.USER_PASSWORD)
+    submit_button.click()
+    # ожидаем перехода на новую страницу
+    WebDriverWait(driver, 10).until(EC.staleness_of(submit_button))
--- locators.py
+++ locators.py
@@ -1,33 +1,47 @@
 # Форма регистрации
 # Поля Input формы регистрации
-NAME_INPUT = "//*[@id='root']/div/main/div/form/fieldset[1]/div/div/input"  # Имя
-EMAIL_INPUT = "//*[@id='root']/div/main/div/form/fieldset[2]/div/div/input"  # e-mail
-PASSWORD_INPUT = "//*[@id='root']/div/main/div/form/fieldset[3]/div/div/input"  # Пароль
+# Имя
+NAME_INPUT = ".//form[contains(@class, 'Auth_form__3qKeq')]//label[text() = 'Имя']/parent::div/input"
+# Email
+EMAIL_INPUT = ".//form[contains(@class, 'Auth_form__3qKeq')]//label[text() = 'Email']/parent::div/input"
+# Пароль
+PASSWORD_INPUT = ".//form[contains(@class, 'Auth_form__3qKeq')]//input[@name='Пароль']"
+# Кнопка submit в форме регистрации
+SUBMIT_BUTTON = ".//form[contains(@class, 'Auth_form__3qKeq')]//button[text() = 'Зарегистрироваться' and contains(@class, 'button_button__33qZ0')]"
+# Тег в котором выводится ошибка при некорректном вводе в поле пароля
+ERROR_MESSAGE = ".//form[contains(@class, 'Auth_form__3qKeq')]//p[contains(@class, 'input__error')]"
 
-SUBMIT_BUTTON = "//*[@id='root']/div/main/div/form/button" # Кнопка submit в форме регистрации
-ERROR_MESSAGE = "input__error"  # Тег в котором выводится ошибка при некорректном вводе в поле пароля
-
-#Форма Авторизации
-ACCOUNT_BUTTON = '//*[@id="root"]/div/main/section[2]/div/button'  # Кнопка "Войти в аккаунт" на на главной
-PROFILE_BUTTON = ".//a[@href='/account']"  # Кнопка "Личный кабинет" на главной станице
-REGISTER_BUTTON = "//a[contains(@class, 'Auth_link__1fOlj')]"  # Кнопка "Войти"ё на странице регистрации
-FORGOT_PASSWORD_BUTTON = "//a[contains(@class, 'Auth_link__1fOlj')]"  # Кнопка "Войти" на странице восстановления пароля
+# Переход на форму авторизации
+# Кнопка "Войти в аккаунт" на на главной
+ACCOUNT_BUTTON = ".//button[text() = 'Войти в аккаунт' and contains(@class, 'button_button__33qZ0')]"
+# Кнопка "Личный кабинет" на главной станице
+PROFILE_BUTTON = ".//a[@href='/account']"
+# Кнопка "Войти" на странице регистрации
+REGISTER_BUTTON = "//a[contains(@class, 'Auth_link__1fOlj')]"
+# Кнопка "Войти" на странице восстановления пароля
+FORGOT_PASSWORD_BUTTON = "//a[contains(@class, 'Auth_link__1fOlj')]"
 
 # Форма авторизации
 # Поле "Email"
-LOGIN_FORM_EMAIL_INPUT = "//*[@id='root']/div/main/div/form/fieldset[1]/div/div/input"
+LOGIN_FORM_EMAIL_INPUT = ".//form[contains(@class, 'Auth_form__3qKeq')]//label[text() = 'Email']/parent::div/input"
 # Поле "Пароль"
-LOGIN_FORM_PASSWORD_INPUT = "//*[@id='root']/div/main/div/form/fieldset[2]/div/div/input"
+LOGIN_FORM_PASSWORD_INPUT = ".//form[contains(@class, 'Auth_form__3qKeq')]//input[@name='Пароль']"
 # Кнопка "Войти"
-LOGIN_FORM_SUBMIT_BUTTON = "//*[@id='root']/div/main/div/form/button"
+LOGIN_FORM_SUBMIT_BUTTON = ".//form[contains(@class, 'Auth_form__3qKeq')]//button[text() = 'Войти' and contains(@class, 'button_button__33qZ0')]"
+
+# Кнопка "Оформить заказ"
+CHECKOUT_BUTTON = ".//button[text() = 'Оформить заказ']"
 
 # Личный кабинет
 # Ссылка "Конструктор" в личном кабинете
-PROFILE_CONSTRUCTOR = "(//a[contains(@class, 'AppHeader_header__link__3D_hX')])[1]"
-PROFILE_LOGOUT = "//button[contains(@class, 'Account_button__14Yp3')]"
+PROFILE_CONSTRUCTOR = ".//p[text() = 'Конструктор' and contains(@class, 'AppHeader_header__linkText__3q_va ')]/parent::a"
+# Кнопка "Выход"
+PROFILE_LOGOUT = ".//button[text() = 'Выход' and contains(@class, 'Account_button__14Yp3')]"
 
 # Конструктор
-# Кнопки категорий
-CONSTRUCTOR_CATEGORIES_BUTTON = "//section[contains(@class, 'BurgerIngredients_ingredients__1N8v2')]//div[1]//div"
-# Заголовок категории в меню
-CONSTRUCTOR_CATEGORIES_TITLE = "//section[contains(@class, 'BurgerIngredients_ingredients__1N8v2')]//div[2]//h2"
+# Таб "Булки"
+CONSTRUCTOR_CATEGORIES_BUTTON_BULKI = ".//section[contains(@class, 'BurgerIngredients_ingredients__1N8v2')]//div[contains(@class, 'tab_tab__1SPyG ')]/span[text() = 'Булки']/parent::div"
+# Таб "Соусы"
+CONSTRUCTOR_CATEGORIES_BUTTON_SOUSI = ".//section[contains(@class, 'BurgerIngredients_ingredients__1N8v2')]//div[contains(@class, 'tab_tab__1SPyG ')]/span[text() = 'Соусы']/parent::div"
+# Таб "Начинки"
+CONSTRUCTOR_CATEGORIES_BUTTON_NACHINKI = ".//section[contains(@class, 'BurgerIngredients_ingredients__1N8v2')]//div[contains(@class, 'tab_tab__1SPyG ')]/span[text() = 'Начинки']/parent::div"
